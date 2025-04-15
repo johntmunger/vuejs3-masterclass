4235519c60249f6c05355d3f6b5a9ecd8fae161d
johntmunger <johntmunger@gmail.com>
2025-04-15 16:07:45 -0600

    create new task table, migrations, seed, types and created a new types template page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient';
 import { ref } from 'vue';
+import type { Tables } from '../../../database.types';
 
-const projects = ref();
+const projects = ref<Tables<'projects'>[] | null>(null);
 (async () => {
   const { data, error } = await supabase.from('projects').select();
 
@@ -17,7 +18,12 @@ const projects = ref();
 <template lang="">
   <div>
     <h1>Projects page</h1>
-    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
-    {{ projects }}
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink> |
+    <RouterLink to="/">Go to Home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>
