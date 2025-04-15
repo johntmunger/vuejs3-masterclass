379c0a30c797528d34b3316e0c7a702069ab94be
johntmunger <johntmunger@gmail.com>
2025-04-15 15:06:30 -0600

    use ref for reactivity to store data locally

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient';
+import { ref } from 'vue';
 
+const projects = ref();
 (async () => {
   const { data, error } = await supabase.from('projects').select();
 
   if (error) console.log(error);
 
-  console.log('Projects: ', data);
+  projects.value = data;
+
+  console.log('projects: ', projects.value);
 })();
 </script>
 
@@ -14,5 +18,6 @@ import { supabase } from '@/lib/supabaseClient';
   <div>
     <h1>Projects page</h1>
     <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
+    {{ projects }}
   </div>
 </template>
