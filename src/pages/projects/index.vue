e1c5b4b4a667b2aaeddddb946bc55e0aa4a8333a
johntmunger <johntmunger@gmail.com>
2025-05-12 12:54:18 -0600

    make the data tavle cells clickable with router link

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,29 +1,58 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
-import { ref } from 'vue';
-import type { Tables } from '../../../database.types';
+import { supabase } from '@/lib/supabaseClient'
+import { h, ref } from 'vue'
+import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
+import { RouterLink } from 'vue-router'
 
-const projects = ref<Tables<'projects'>[] | null>(null);
-(async () => {
-  const { data, error } = await supabase.from('projects').select();
+const projects = ref<Tables<'projects'>[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
 
-  if (error) console.log(error);
+  if (error) console.log(error)
 
-  projects.value = data;
+  projects.value = data
 
-  console.log('projects: ', projects.value);
-})();
+  console.log('projects: ', projects.value)
+})()
+
+const columns: ColumnDef<Tables<'projects'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.slug}`,
+          class: 'text-left font-medium hover:bg-muted block w-full'
+        },
+        () => row.getValue('name')
+      )
+    }
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    }
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators'))
+      )
+    }
+  }
+]
 </script>
 
-<template lang="">
-  <div>
-    <h1>Projects page</h1>
-    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink> |
-    <RouterLink to="/">Go to Home</RouterLink>
-    <ul>
-      <li v-for="project in projects" :key="project.id">
-        {{ project.name }}
-      </li>
-    </ul>
-  </div>
+<template>
+  <DataTable v-if="projects" :columns="columns" :data="projects" />
 </template>
