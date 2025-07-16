7b3219b75e5deec489fd804a198959741edac8e8
johntmunger <johntmunger@gmail.com>
2025-07-16 13:35:55 -0600

    add a register page with session and local profile auth to supabase

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,60 +1,35 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div
-    class="mx-auto w-full flex justify-center items-center p-10 text-center -mt-10 min-h-[90vh] h-full"
-  >
-    <Card class="max-w-sm w-full mx-auto h-full">
+  <div class="mx-auto flex w-full justify-center items-center p-10 text-center -mt-20 min-h-[90vh]">
+    <Card class="max-w-sm w-full mx-auto">
       <CardHeader>
-        <CardTitle class="text-2xl"> Register </CardTitle>
-        <CardDescription> Create a new account </CardDescription>
+        <CardTitle class="text-2xl"> Login </CardTitle>
+        <CardDescription> Login to your account </CardDescription>
       </CardHeader>
       <CardContent>
         <div class="flex flex-col gap-4 mb-4 justify-center items-center">
           <Button variant="outline" class="w-full"> Register with Google </Button>
           <Separator label="Or" />
         </div>
+
         <form class="grid gap-4">
           <div class="grid gap-2">
-            <Label id="username" class="text-left">Username</Label>
-            <Input id="username" type="text" placeholder="johndoe19" required />
-          </div>
-          <div class="flex flex-col sm:flex-row justify-between gap-4">
-            <div class="grid gap-2">
-              <Label id="first_name" class="text-left">First Name</Label>
-              <Input id="first_name" type="text" placeholder="John" required />
-            </div>
-            <div class="grid gap-2">
-              <Label id="last_name" class="text-left">Last Name</Label>
-              <Input id="last_name" type="text" placeholder="Doe" required />
-            </div>
-          </div>
-          <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input id="email" type="email" placeholder="johndoe19@example.com" required />
+            <Input type="email" placeholder="johndoe19@example.com" required />
           </div>
-
           <div class="grid gap-2">
-            <Label id="password" class="text-left">Password</Label>
-            <Input id="password" type="password" placeholder="*****" autocomplete required />
+            <div class="flex items-center">
+              <Label id="password">Password</Label>
+              <a href="#" class="inline-block ml-auto text-xs underline"> Forgot your password? </a>
+            </div>
+            <Input id="password" type="password" autocomplete required />
           </div>
-
-          <div class="grid gap-2">
-            <Label id="confirm_password" class="text-left">Confirm Password</Label>
-            <Input
-              id="confirm_password"
-              type="password"
-              placeholder="*****"
-              autocomplete
-              required
-            />
-          </div>
-          <Button type="submit" class="w-full"> Register </Button>
-          <!-- <Button variant="outline" class="w-full"> Login with Google </Button> -->
+          <Button type="submit" class="w-full"> Login </Button>
         </form>
         <div class="mt-4 text-sm text-center">
-          Already have an account?
-          <RouterLink to="/login" class="underline"> Login </RouterLink>
+          Don't have an account?
+          <RouterLink to="/register" class="underline"> Register </RouterLink>
         </div>
       </CardContent>
     </Card>
