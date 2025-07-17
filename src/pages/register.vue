c816e26d48cf7096821ec8a198148614ea0cfc49
johntmunger <johntmunger@gmail.com>
2025-07-17 14:43:20 -0600

    create login and register and allow for authentication with redirects

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
+import { register } from '@/utils/supaAuth';
 
 const formData = ref({
   username: '',
@@ -13,25 +13,8 @@ const formData = ref({
 const router = useRouter();
 
 const signup = async () => {
-
-  const { data, error } = await supabase.auth.signUp({
-  email: formData.value.email,
-  password: formData.value.password,
-})
-
-  if (error) return console.log(error)
-
-  if(data.user) {
-    const { error} = await supabase.from('profiles').insert({
-    id: data.user?.id,
-    username: formData.value.username,
-    full_name: formData.value.firstName.concat(' ', formData.value.lastName)
-  })
-
-    if(error) console.log('Profiles err:', error)
-  }
-
-  router.push('/')
+  const isRegistered = await register(formData.value)
+    if(isRegistered) router.push('/')
 }
 
 
